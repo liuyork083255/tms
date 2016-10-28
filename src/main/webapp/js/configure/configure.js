5c36af1678db3d476218c52e8e9265eb459f0383
york <york>
2016-10-28 14:40:21 +0800

    js改动

--- src/main/webapp/js/configure/configure.js
+++ src/main/webapp/js/configure/configure.js
@@ -1,8 +1,25 @@
 jQuery.extend({
 	//初始化方法，如果要引入多个js文件，则可以部署在这里，启动jquery的时候首先调用该初始化方法即可
 	init:function(){
-		$("html").append("<script type=\"text/javascript\" src=\"myjs/extends.js\"></script>");
+		$("html").append("<script type='text/javascript' src='myjs/extends.js'></script>");
+		/*引入easyui的核心js文件*/
+		$("html").append("<script type='text/javascript' src='js/jquery-easyui-1.5/jquery.easyui.min.js' charset='utf-8'></script>");
 		
+		/*引入easyui的默认主题*/
+		$("html").append("<link rel='stylesheet' href='js/jquery-easyui-1.5/themes/default/easyui.css' type='text/css' charset='utf-8'></link>");
+		
+		/*引入easyui的小图标*/
+		$("html").append("<link rel='stylesheet' href='js/jquery-easyui-1.5/themes/icon.css' type='text/css' charset='utf-8'></link>");
+	
+		/*引入easyui的中文js包*/
+		$("html").append("<script type='text/javascript' src='js/jquery-easyui-1.5/locale/easyui-lang-zh_CN.js' charset='utf-8'></script>");
+		
+		
+		
+		
+		
+		
+	
 	}
   
 });
