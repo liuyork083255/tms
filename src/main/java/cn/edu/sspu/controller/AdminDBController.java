7001b99c2b676dec05a21dd9097f6f52aab2525d
york <york>
2016-11-15 18:35:07 +0800

    controller编程

--- src/main/java/cn/edu/sspu/controller/AdminDBController.java
+++ src/main/java/cn/edu/sspu/controller/AdminDBController.java
@@ -1,5 +1,6 @@
 package cn.edu.sspu.controller;
 
+import java.time.temporal.IsoFields;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,9 +13,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
 
+import cn.edu.sspu.models.Input;
 import cn.edu.sspu.models.Model;
+import cn.edu.sspu.models.Select;
 import cn.edu.sspu.models.Table;
+import cn.edu.sspu.models.Textarea;
 import cn.edu.sspu.pojo.Json;
+import cn.edu.sspu.utils.AdminUtils;
 
 
 
@@ -22,7 +27,8 @@ import cn.edu.sspu.pojo.Json;
 @RequestMapping("/admin/db")
 public class AdminDBController {
 	
-	
+	/*这是测试对象，可以删除*/
+	private Model model;
 	
 	/*该方法是接收一个tableName参数，判断数据库是否存在，存在则Json返回false，否则返回true*/
 	@ResponseBody
@@ -49,7 +55,8 @@ public class AdminDBController {
 		Json json = new Json();
 		if (model != null) {
 			System.out.println(JSON.toJSONString(model, true));
-			json.setSuccess(true);
+			model.setTable_id("随意一个测试table_id:001");
+			json.setSuccess(true);this.model = model;
 			json.setMsg("model保存成功");
 		}else{
 			json.setSuccess(false);
@@ -91,6 +98,59 @@ public class AdminDBController {
 		}
 		return json;
 	}
+	
+	/*该方法接收一个table_id参数，返回model对象*/
+	@ResponseBody
+	@RequestMapping("/getmodelbytableid")
+	public Model getModelByTableId(String table_id){
+		if(this.model != null){
+			return this.model;
+		}
+		return null;
+	}
+	
+	/*该方法没有参数，返回model模板，用于前端编辑修改*/
+	@ResponseBody
+	@RequestMapping("/getmodel")
+	public List<Object> getModel(String table_id){
+		if(this.model != null){
+			System.out.println(this.model);
+			return AdminUtils.modelToList(model,table_id);
+		}
+		else{
+			System.out.println("model is null");
+			return null;
+		}
+	}
+	
+	/*该方法是接收一个input对象，然后保存入库，这个对象必须有table_id，不然无法保存*/
+	@ResponseBody
+	@RequestMapping("/saveinput")
+	public void saveInput(@RequestBody Input input){
+		System.out.println(JSON.toJSONString(input, true));
+	}
+	/*该方法是接收一个select对象，然后保存入库，这个对象必须有table_id，不然无法保存*/
+	@ResponseBody
+	@RequestMapping("/saveselect")
+	public void saveSelect(@RequestBody Select select){
+		System.out.println(JSON.toJSONString(select, true));
+	}
+	/*该方法是接收一个textarea对象，然后保存入库，这个对象必须有table_id，不然无法保存*/
+	@ResponseBody
+	@RequestMapping("/savetextarea")
+	public void saveTextarea(@RequestBody Textarea textarea){
+		System.out.println(JSON.toJSONString(textarea, true));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
