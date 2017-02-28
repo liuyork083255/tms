6bf7d740d96a04c678dd73b2166bc74e354aa340
liuyork083255 <18721816191@163.com>
2017-02-28 10:48:46 +0800

    完善UserController类用户登录校验

--- src/main/java/cn/edu/sspu/controller/UserController.java
+++ src/main/java/cn/edu/sspu/controller/UserController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import cn.edu.sspu.exception.ServiceException;
 import cn.edu.sspu.models.User;
+import cn.edu.sspu.pojo.Json;
 import cn.edu.sspu.service.UserService;
 
 @Controller
@@ -14,11 +17,39 @@ public class UserController {
 	@Autowired
 	private UserService userService = null;
 	
+	/**
+	 * 用户登录
+	 * @param name
+	 * @return
+	 */
+	@ResponseBody
 	@RequestMapping("/login")
-	public void loginUser(String name){
-		System.out.println("controller is ok");
-		User user = userService.selectUserByName(name);
-		System.out.println(user);
+	public Json loginUser(String name,String password){
+		
+		Json json = new Json();
+		if(name == null || password == null){
+			json.setMsg("用户名 or 密码 请求参数为空");
+			json.setSuccess(false);
+			return json;
+		}
+		
+		User user = null;
+		try {
+			user = userService.selectUserByNameAndPassword(name, password);
+		} catch (ServiceException e) {
+			json.setMsg(e.getMessage());
+			json.setSuccess(false);
+			return json;
+		}catch (Exception e){
+			json.setMsg(e.getMessage());
+			json.setSuccess(false);
+			return json;
+		}
+		json.setSuccess(true);
+		json.setObj(user);
+		System.out.println(json);
+		return json;
 	}
+	
 
 }
