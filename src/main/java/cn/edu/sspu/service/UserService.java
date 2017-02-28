02c1668a1d7a8cf8156ef6f0bed08be45d5b96d4
liuyork083255 <18721816191@163.com>
2017-02-28 10:50:37 +0800

    完善service 层用户登录模块

--- src/main/java/cn/edu/sspu/service/UserService.java
+++ src/main/java/cn/edu/sspu/service/UserService.java
@@ -1,10 +1,15 @@
 package cn.edu.sspu.service;
 
+import cn.edu.sspu.exception.ServiceException;
 import cn.edu.sspu.models.User;
 
 
 public interface UserService {
 	
-	public User selectUserByName(String name);
-
+	public User selectUserByName(String name) throws ServiceException;
+	
+	public User selectUserById(String user_id) throws ServiceException;
+	
+	public User selectUserByNameAndPassword(String name,String password) throws ServiceException;
+	
 }
--- src/main/java/cn/edu/sspu/service/serviceImpl/UserServieceImpl.java
+++ src/main/java/cn/edu/sspu/service/serviceImpl/UserServieceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cn.edu.sspu.dao.mapper.UserMapper;
+import cn.edu.sspu.exception.ExceptionType;
+import cn.edu.sspu.exception.ServiceException;
 import cn.edu.sspu.models.User;
 import cn.edu.sspu.service.UserService;
 
@@ -13,9 +15,32 @@ public class UserServieceImpl implements UserService{
 	@Autowired
 	private UserMapper userMapper = null ;
 
-	public User selectUserByName(String name) {
-		System.out.println("the userMapper is" + userMapper);
-		return userMapper.selectUserByName(name);
+	public User selectUserByName(String name) throws ServiceException {
+		User user = userMapper.selectUserByName(name);
+		
+		if(user == null){
+			throw new ServiceException("按name查找user为空");
+		}
+		return user;
 	}
-
+	
+	public User selectUserById(String user_id) throws ServiceException {
+		User user = userMapper.selectUserByName(user_id);
+		
+		if(user == null){
+			throw new ServiceException("按id查找user为空");
+		}
+		
+		return user;
+	}
+	
+	public User selectUserByNameAndPassword(String name,String password) throws ServiceException{
+		User user = userMapper.selectUserByNameAndPassword(name,password);
+		
+		if(user == null){
+			throw new ServiceException("按name和password查找user为空");
+		}
+		return user;
+		
+	}
 }
